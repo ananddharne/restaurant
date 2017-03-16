5f98817d6e92090fb5c2f3b70f836ecf6996e6d8
ananddharne <anand.dharne24@gmail.com>
2017-03-16 11:39:48 -0500

    last chane

--- Json-server/public/scripts/services.js
+++ Json-server/public/scripts/services.js
@@ -1,7 +1,7 @@
 'use strict'
 
 angular.module('confusionApp')
-		.constant("baseURL","http://localhost:3000/")
+		.constant("baseURL","https://localhost:3000/")
 
         .service('menuFactory', ['$http', 'baseURL', function($http,baseURL) {
     
