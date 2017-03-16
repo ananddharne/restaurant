3b7ef6329ebc5255e201449b6f467d970d480ac8
ananddharne <anand.dharne24@gmail.com>
2017-03-16 10:09:02 -0500

    Adding my existing site1.

---
+++ scripts/controllers.js
@@ -0,0 +1,190 @@
+'use strict';
+ 
+
+   angular.module('confusionApp')
+
+         .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = true;
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+            $scope.dishes= [];
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "Hyderabadi Biryani";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "Goat Specialties";
+                }
+
+                else if (setTab === 5) {
+                    $scope.filtText = "Chicken Specialties";
+                }
+
+                else if (setTab === 6) {
+                    $scope.filtText = "Veg";
+                }
+
+                else if (setTab === 7) {
+                    $scope.filtText = "Rice and Roti";
+                }
+
+                else if (setTab === 7) {
+                    $scope.filtText = "dessert";
+                }
+
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:true, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+
+
+        // #####################- Repalacing routeParams with stateParams this is how we supply parameters to controllers.
+
+        // .controller('dishDetailController', ['$scope','$routeParams', 'menuFactory', function($scope,$routeParams, menuFactory) {
+
+        //     $scope.dish = [menuFactory.getDish(parseInt($routeParams.id,10))];
+        //     $scope.filtext = '';
+
+           
+        // }])
+
+            .controller('dishDetailController', ['$scope','$stateParams', 'menuFactory', function($scope,$stateParams, menuFactory) {
+
+             $scope.dish = [];
+             $scope.showDish = false;
+             $scope.message="Loading ...";
+             menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = [response.data];
+                    $scope.showDish=true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
+
+            $scope.filtext = '';
+
+           
+        }])
+
+
+
+
+        .controller('FeedbackController_1', ['$scope', function($scope) {
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+
+            $scope.feedback = {rating:"5" , comment:"", author:"",  date:""};
+ 
+           
+            $scope.submitComment = function () {
+            
+
+                $scope.feedback.date = new Date().toISOString();
+               
+                $scope.dish[0].comments.push($scope.feedback)
+
+                $scope.feedbackForm_1.$setPristine();
+
+                $scope.feedback = {rating:"5" , comment:"", author:"",  date:""};
+                
+            }
+        }])
+
+
+        .controller('AboutController', ['$scope', 'corporateFactory', function($scope,corporateFactory) {
+
+                $scope.leaders_details = corporateFactory.getLeaders();
+
+            
+        }])
+
+        // .controller('IndexController', ['$scope','menuFactory', function ($scope,menuFactory) {
+        .controller('IndexController',['$scope','corporateFactory','menuFactory',function($scope,corporateFactory,menuFactory){
+            
+                $scope.month_pro = [menuFactory.getpromotion(0)];
+
+                $scope.featured = [];
+                $scope.showDish = false;
+                menuFactory.getDish(0)
+                .then(
+                    function(response){
+                        $scope.featured = [response.data];
+                        $scope.showDish = true;
+                    },
+                     function(response) {
+                                $scope.message = "Error: "+response.status + " " + response.statusText;
+                            }
+                );
+
+                $scope.leader_details  = [corporateFactory.getLeader(3)];
+        }])
+
+
+;
